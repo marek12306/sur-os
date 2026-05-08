44cf2a134ee1f63784220ddbc72e3af38a8efa60
deepivin <marek12306@gmail.com>
2026-05-08 14:20:10 +0200

    babababfdabad

--- files/scripts/i915-sriov.sh
+++ files/scripts/i915-sriov.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ouex pipefail
 
-dnf install -y akmods sysfsutils rpm-build rpmdevtools curl jq kmodtool dnf-plugins-core
+dnf install -y akmods sysfsutils rpm-build rpmdevtools kmodtool dnf-plugins-core jq curl
 
 dnf copr enable -y sihawken/akmod-i915-sriov
 
@@ -10,15 +10,18 @@ dnf download --source akmod-i915-sriov
 
 rpm -ivh *.src.rpm
 
+rm -f /root/rpmbuild/SOURCES/*.tar.gz
+
 LATEST_COMMIT=$(curl -s https://api.github.com/repos/strongtz/i915-sriov-dkms/commits/master | jq -r .sha)
+SHORT_COMMIT=${LATEST_COMMIT:0:7}
 
 sed -i "s/%global commit .*/%global commit $LATEST_COMMIT/" /root/rpmbuild/SPECS/*.spec
+sed -i "s/%global shortcommit .*/%global shortcommit $SHORT_COMMIT/" /root/rpmbuild/SPECS/*.spec
+sed -i '/Requires:.*-common/d' /root/rpmbuild/SPECS/*.spec
 
 spectool -g -R /root/rpmbuild/SPECS/*.spec
 
-rpmbuild -ba /root/rpmbuild/SPECS/*.spec
-
-dnf install -y /root/rpmbuild/RPMS/*/*.rpm --setopt=tsflags=noscripts
+rpmbuild -bs /root/rpmbuild/SPECS/*.spec
 
 mkdir -p /tmp/akmods-build
 cp /root/rpmbuild/SRPMS/*.src.rpm /tmp/akmods-build/
@@ -29,5 +32,5 @@ KERNEL_VERSION=$(ls /lib/modules | grep -E '^[0-9]\.' | head -n 1)
 cd /tmp/akmods-build
 su -s /bin/bash akmods -c "akmodsbuild --kernels ${KERNEL_VERSION} /tmp/akmods-build/*.src.rpm"
 
-dnf install -y /tmp/akmods-build/*.rpm
+dnf install -y /tmp/akmods-build/*.rpm --setopt=tsflags=noscripts
 
