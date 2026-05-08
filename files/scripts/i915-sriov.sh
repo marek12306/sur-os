eaf376cfdec465679cc0bbb40fd7b582b10ab8f4
deepivin <marek12306@gmail.com>
2026-05-08 13:25:14 +0200

    aaaaaaaaaaaaaaaaaa

--- files/scripts/i915-sriov.sh
+++ files/scripts/i915-sriov.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 set -ouex pipefail
 
-echo "=== i915-sriov ==="
+dnf install -y rpm-build rpmdevtools git curl jq kmodtool
 
-dnf install -y --setopt=tsflags=noscripts akmod-i915-sriov
+mkdir -p /root/rpmbuild/{SOURCES,SPECS}
+git clone https://github.com/matte23/akmod-i915-sriov.git /tmp/akmod-pkg
+cp /tmp/akmod-pkg/* /root/rpmbuild/SOURCES/
+mv /root/rpmbuild/SOURCES/i915-sriov.spec /root/rpmbuild/SPECS/
+
+LATEST_COMMIT=$(curl -s https://api.github.com/repos/strongtz/i915-sriov-dkms/commits/master | jq -r .sha)
+
+sed -i "s/%global commit .*/%global commit $LATEST_COMMIT/" /root/rpmbuild/SPECS/i915-sriov.spec
+
+spectool -g -R /root/rpmbuild/SPECS/i915-sriov.spec
+rpmbuild -ba /root/rpmbuild/SPECS/i915-sriov.spec
+
+dnf install -y /root/rpmbuild/RPMS/x86_64/i915-sriov-*.rpm /root/rpmbuild/RPMS/x86_64/akmod-i915-sriov-*.rpm --setopt=tsflags=noscripts
 
 mkdir -p /tmp/akmods-build
-cp /usr/src/akmods/i915-sriov-kmod*.src.rpm /tmp/akmods-build/
+cp /root/rpmbuild/SRPMS/*.src.rpm /tmp/akmods-build/
 chown -R akmods:akmods /tmp/akmods-build
 
 KERNEL_VERSION=$(ls /lib/modules | grep -E '^[0-9]\.' | head -n 1)
 
-su -s /bin/bash akmods -c "akmodsbuild --kernels ${KERNEL_VERSION} /tmp/akmods-build/i915-sriov-kmod*.src.rpm"
+cd /tmp/akmods-build
+su -s /bin/bash akmods -c "akmodsbuild --kernels ${KERNEL_VERSION} /tmp/akmods-build/*.src.rpm"
 
 dnf install -y /tmp/akmods-build/*.rpm
-
-echo "=== i915-sriov done ==="
-
