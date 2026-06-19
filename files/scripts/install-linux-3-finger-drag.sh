c60747dc380e320ec25b73de2cff2ddc39218f30
deepivin <marek12306@gmail.com>
2026-06-19 14:17:30 +0200

    feat: default config for linux-3-finger-drag

--- files/scripts/install-linux-3-finger-drag.sh
+++ files/scripts/install-linux-3-finger-drag.sh
@@ -25,8 +25,13 @@ cp ./60-uinput.rules /usr/lib/udev/rules.d/
 # Provide uaccess to touchpads so the program can read from them without the user being in the 'input' group.
 echo 'SUBSYSTEM=="input", ENV{ID_INPUT_TOUCHPAD}=="1", TAG+="uaccess"' > /usr/lib/udev/rules.d/61-touchpad-uaccess.rules
 
+# Install default configuration template
+mkdir -p /usr/share/linux-3-finger-drag
+cp ./3fd-config.json /usr/share/linux-3-finger-drag/
+
 # Install systemd user service
 mkdir -p /usr/lib/systemd/user
+sed -i '/^ExecStart=/i ExecStartPre=/bin/sh -c "mkdir -p %h/.config/linux-3-finger-drag; [ -f %h/.config/linux-3-finger-drag/3fd-config.json ] || cp /usr/share/linux-3-finger-drag/3fd-config.json %h/.config/linux-3-finger-drag/3fd-config.json"' three-finger-drag.service
 cp three-finger-drag.service /usr/lib/systemd/user/
 systemctl --global enable three-finger-drag.service
 
